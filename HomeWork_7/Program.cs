aa9892f512a206d6c2c1f558a8aebb776ed984a2
Hadz_xt61@student.itstep.kz <Hadz_xt61@student.itstep.kz>
2021-01-12 19:35:41 +0600

    Добавьте файлы проекта.

---
+++ HomeWork_7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HomeWork_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rangeOfArray = new RangeOfArray();
+
+            string incomingNumber = Console.ReadLine();
+            int temporaryLowerNumber;
+            int.TryParse(incomingNumber, out temporaryLowerNumber);
+            rangeOfArray.LowerIndex = temporaryLowerNumber;
+
+            incomingNumber = Console.ReadLine();
+            int temporaryUpperNumber;    
+            int.TryParse(incomingNumber, out temporaryUpperNumber);
+            rangeOfArray.UpperIndex = temporaryUpperNumber;
+
+            incomingNumber = Console.ReadLine();
+            int.TryParse(incomingNumber, out var length);
+
+            int[] arrayOfNumbers = new int[10];
+            var numbers = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var range = new Range(temporaryLowerNumber, temporaryUpperNumber);
+            arrayOfNumbers = numbers[range];
+
+            Console.WriteLine();
+
+            for (int i = 0; i < arrayOfNumbers.Length; i++)
+            {
+                Console.Write(arrayOfNumbers[i]);
+            }
+        }
+    }
+}
---
+++ HomeWork_7/RangeOfArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork_7
+{
+    public class RangeOfArray
+    {
+        public int LowerIndex { set; get; }
+        public int UpperIndex { set; get; }
+
+        public RangeOfArray() { } 
+
+        public RangeOfArray(int lowerIndex, int upperIndex)
+        {
+            this.LowerIndex = lowerIndex;
+            this.UpperIndex = upperIndex;
+        }
+    }
+}
